c1c2be1de92afb845f664c164b9ccd02e2c1b7f1
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2020-01-09 19:02:04 +0100

    Simple ChaCha20 encryption example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use sodiumoxide::crypto::stream::xchacha20;
+use base64;
+
+
 fn main() {
-	// Code here
+	let key = xchacha20::gen_key();
+	let nonce = xchacha20::gen_nonce();
+	let plaintext = b"some data";
+	let ciphertext = xchacha20::stream_xor(plaintext, &nonce, &key);
+	let _their_plaintext = xchacha20::stream_xor(&ciphertext, &nonce, &key);
+
+	println!("{}", base64::encode(&ciphertext));
 }
