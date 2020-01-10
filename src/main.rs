6e34a1c271536bba76b14a71e7f8645a2dd942eb
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2020-01-10 15:17:51 +0100

    Add ChaCha20 key importing/exporting using serde

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use sodiumoxide::crypto::stream::xchacha20;
-use base64;
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use sodiumoxide::crypto::stream::chacha20;
+
+
+fn chacha_import(key_json: &String) -> chacha20::Key {
+	serde_json::from_str(&key_json).unwrap()
+}
+
+fn chacha_export(key: &chacha20::Key) -> String {
+	serde_json::to_string(&key).unwrap()
+}
+
 
 
 fn main() {
-	let key = xchacha20::gen_key();
-	let nonce = xchacha20::gen_nonce();
-	let plaintext = b"some data";
-	let ciphertext = xchacha20::stream_xor(plaintext, &nonce, &key);
-	let _their_plaintext = xchacha20::stream_xor(&ciphertext, &nonce, &key);
+	let key = chacha_import(&String::from("[213,216,159,212,251,40,38,202,228,225,175,168,131,10,84,14,87,199,173,30,210,26,22,1,28,153,192,72,163,38,159,222]"));
+	// let key: chacha20::Key = chacha20::gen_key();
+	let nonce = chacha20::Nonce::from_slice(b"verygood").unwrap();
 
-	println!("{}", base64::encode(&ciphertext));
+	let plaintext = b"some data";
+	let ciphertext = chacha20::stream_xor(plaintext, &nonce, &key);
+	let their_plaintext = chacha20::stream_xor(&ciphertext, &nonce, &key);
+
+	println!("{:?}", ciphertext);
 }
