047a4e91eb87752af4ca0cf87eef11984e4b7dcf
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2020-01-10 20:17:10 +0100

    Use base64 for key encoding instead of serde_json

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod files;
 
 
 fn main() {
-	let key = cryptography::chacha_import("[213,216,159,212,251,40,38,202,228,225,175,168,131,10,84,14,87,199,173,30,210,26,22,1,28,153,192,72,163,38,159,222]");
+	let key = cryptography::chacha_import("1dif1PsoJsrk4a+ogwpUDlfHrR7SGhYBHJnASKMmn94=");
 	let nonce = chacha20::Nonce::from_slice(b"verygood").unwrap();
 
 	let plaintext = files::read_file("victim_files/test.txt").unwrap();
