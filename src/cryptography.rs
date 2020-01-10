569a4a6fb571be7d54353cbd63f7b8ce8be12565
Sam van der Kris <samvdkris@gmail.com>
2020-01-10 20:17:10 +0100

    Use base64 for key encoding instead of serde_json

--- src/cryptography.rs
+++ src/cryptography.rs
@@ -1,10 +1,14 @@
 use sodiumoxide::crypto::stream::chacha20;
+use base64;
 
 
-pub fn chacha_import(key_json: &'static str) -> chacha20::Key {
-	serde_json::from_str(&key_json).unwrap()
+
+pub fn chacha_import(key_base64: &'static str) -> chacha20::Key {
+	let decoded = base64::decode(&key_base64).unwrap();
+	chacha20::Key::from_slice(&decoded).unwrap()
 }
 
 pub fn chacha_export(key: &chacha20::Key) -> String {
-	serde_json::to_string(&key).unwrap()
+	// serde_json::to_string(&key).unwrap()
+	base64::encode(&key)
 }
