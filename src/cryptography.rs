1c4507ed0ab1bfc0b3e716c2909209b45dd6aa7e
Sam van der Kris <samvdkris@gmail.com>
2020-01-26 20:42:28 +0100

    Add networking module and properly document all module functions

--- src/cryptography.rs
+++ src/cryptography.rs
@@ -2,13 +2,22 @@ use sodiumoxide::crypto::stream::chacha20;
 use base64;
 
 
-
+/*
+	Import a ChaCha20 key from a base64 string
+	Example
+		let key = cryptography::chacha_import("1dif1PsoJsrk4a+ogwpUDlfHrR7SGhYBHJnASKMmn94=");
+*/
 pub fn chacha_import(key_base64: &'static str) -> chacha20::Key {
 	let decoded = base64::decode(&key_base64).unwrap();
 	chacha20::Key::from_slice(&decoded).unwrap()
 }
 
+
+/*
+	Export a ChaCha20 key to a base64 string
+	Example
+		let key_base64 = cryptography::chacha_export(&key);
+*/
 pub fn chacha_export(key: &chacha20::Key) -> String {
-	// serde_json::to_string(&key).unwrap()
 	base64::encode(&key)
 }
---
+++ src/networking.rs
@@ -0,0 +1,24 @@
+/*
+	Sends a GET request, returns a result that can be unwrapped
+	Example
+		let body = networking::get("https://example.com").unwrap();
+*/
+pub fn get(url: &'static str) -> Result<String, Box<dyn std::error::Error>> {
+	let body = reqwest::blocking::get(url)?
+		.text()?;
+	Ok(body)
+}
+
+
+/*
+	Sends a POST request, doesn't return anything
+	Example
+		post("https://example.com", vec![("key", "value")])
+*/
+pub fn post(url: &'static str, params: &Vec<(&'static str, &'static str)>) {
+	let client = reqwest::blocking::Client::new();
+	client.post(url)
+		.form(&params)
+		.send()
+		.expect("An error occurred while trying to send a POST request");
+}
