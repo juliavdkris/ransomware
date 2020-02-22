128a33758677dc1feeeea89f5a196ff9da89801d
Sam van der Kris <samvdkris@gmail.com>
2020-02-22 14:27:25 +0100

    List all files in victim_files directory and encrypt them

--- src/files.rs
+++ src/files.rs
@@ -4,15 +4,27 @@ use std::fs::File;
 
 
 /*
+	List the files in a given directory
+	Example
+		let files = list_files("dirname").unwrap();
+*/
+pub fn list_files(dirname: &'static str) -> io::Result<Vec<std::path::PathBuf>> {
+	let entries = std::fs::read_dir(&dirname)?
+		.map(|res| res.map(|e| e.path()))
+		.collect::<Result<Vec<_>, io::Error>>()?;
+	Ok(entries)
+}
+
+
+/*
 	Read a file to a bytevector
 	Example
 		let text = files::read_file("test.txt").unwrap();
 */
-pub fn read_file(filename: &'static str) -> io::Result<Vec<u8>> {
+pub fn read_file(filename: &str) -> io::Result<Vec<u8>> {
 	let mut f = File::open(filename)?;
 	let mut buffer = Vec::new();
 
-	// read the whole file
 	f.read_to_end(&mut buffer)?;
 	Ok(buffer)
 }
@@ -23,10 +35,9 @@ pub fn read_file(filename: &'static str) -> io::Result<Vec<u8>> {
 	Example
 		files::write_file("test.txt", bytevector).expect("Error writing file.");
 */
-pub fn write_file(filename: &'static str, content: Vec<u8>) -> io::Result<()> {
+pub fn write_file(filename: &str, content: &Vec<u8>) -> io::Result<()> {
 	let mut buffer = File::create(filename)?;
 
-	// Writes some prefix of the byte string, not necessarily all of it.
 	buffer.write(&content)?;
 	Ok(())
 }
